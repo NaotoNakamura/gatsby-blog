a7aff129f6d6058d44debdb82eefd8bbd205f859
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-08-25 21:08:31 +0900

    add link contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,24 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
+  siteMetadata: {
+    title: "ブログタイトル",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+  ],
 }
