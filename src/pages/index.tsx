201cc9e1fa5ad4a6b242fc5a85a1c4d7a7dd89a9
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-08-26 10:18:24 +0900

    get article data from contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 import "../styles/index.css"
+import { graphql, PageProps } from "gatsby"
 
-const Home: React.FC = () => {
+const Home: React.FC<PageProps<GatsbyTypes.Query>> = ({ data }) => {
   return (
     <Layout>
       <div className="articles">
         <h2 className="articles_index">記事一覧</h2>
         <ul className="articles__list">
-          <li className="articles__list__item">
-            <div className="article__img"></div>
-            <div className="article__content">
-              <h3>記事1タイトル</h3>
-              <div className="article__discription">記事1の説明</div>
-            </div>
-          </li>
-          <li className="articles__list__item">
-            <div className="article__img"></div>
-            <div className="article__content">
-              <h3>記事2タイトル</h3>
-              <div className="article__discription">記事2の説明</div>
-            </div>
-          </li>
+          {data.allContentfulArticle.edges.map(edge => {
+            return (
+              <li className="articles__list__item">
+                <img
+                  src={edge.node.image?.file?.url}
+                  className="article__img"
+                ></img>
+                <div className="article__content">
+                  <h3>{edge.node.title}</h3>
+                  <div className="article__discription">
+                    {edge.node.description?.description}
+                  </div>
+                </div>
+              </li>
+            )
+          })}
         </ul>
       </div>
     </Layout>
@@ -29,3 +32,26 @@ const Home: React.FC = () => {
 }
 
 export default Home
+
+export const query = graphql`
+  query allContentfulArticle {
+    allContentfulArticle {
+      edges {
+        node {
+          title
+          image {
+            title
+            file {
+              url
+            }
+          }
+          description {
+            description
+          }
+          slug
+          updatedAt(locale: "ja-JP", formatString: "YYYY年MM月DD日")
+        }
+      }
+    }
+  }
+`
