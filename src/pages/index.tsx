da0f48fdfd8e2c7a6d5c5f81348d4b9a5e7138cf
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-08-26 15:38:32 +0900

    modify index styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,33 @@
 import React from "react"
 import Layout from "../components/layout"
 import "../styles/index.css"
-import { graphql, PageProps } from "gatsby"
+import { graphql, PageProps, Link } from "gatsby"
 
 const Home: React.FC<PageProps<GatsbyTypes.Query>> = ({ data }) => {
   return (
     <Layout>
       <div className="articles">
-        <h2 className="articles_index">記事一覧</h2>
-        <ul className="articles__list">
+        <h2 className="articles-index">記事一覧</h2>
+        <ul className="articles-list">
           {data.allContentfulArticle.edges.map(edge => {
             return (
-              <li className="articles__list__item">
-                <img
-                  src={edge.node.image?.file?.url}
-                  className="article__img"
-                ></img>
-                <div className="article__content">
-                  <h3>{edge.node.title}</h3>
-                  <div className="article__discription">
-                    {edge.node.description?.description}
+              <Link
+                to={`/article/${edge.node.slug}/`}
+                className="articles-link"
+              >
+                <li className="articles-item">
+                  <img
+                    src={edge.node.image?.file?.url}
+                    className="articles-img"
+                  ></img>
+                  <div className="articles-content">
+                    <h3>{edge.node.title}</h3>
+                    <div className="articles-discription">
+                      {edge.node.description?.description}
+                    </div>
                   </div>
-                </div>
-              </li>
+                </li>
+              </Link>
             )
           })}
         </ul>
